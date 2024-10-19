5723eba8a1135c0ac42429cdbca2c8021a058a8b
Abhishek <68496907+abhiraftaar007@users.noreply.github.com>
2024-10-19 14:07:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+const questions = [
+  {
+    que: "Which of the following is a markup language?",
+    a: "HTML",
+    b: "CSS",
+    c: "JavaScript",
+    d: "PHP",
+    correct: "a",
+  },
+  {
+    que: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
+  {
+    que: "What does CSS stands for?",
+    a: "HyperText Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats",
+    correct: "b",
+  },
+];
+
+let index = 0;
+let total = questions.length;
+let right = 0,
+  wrong = 0;
+const quesBox = document.getElementById("quesBox");
+const optionInputs = document.querySelectorAll(".options");
+
+const laodQuestion = () => {
+  if (index == total) {
+    return endQuiz();
+  }
+  reset();
+  const data = questions[index];
+  // console.log(data);
+  quesBox.innerText = `${index + 1}) ${data.que}`;
+  optionInputs[0].nextElementSibling.innerText = data.a;
+  optionInputs[1].nextElementSibling.innerText = data.b;
+  optionInputs[2].nextElementSibling.innerText = data.c;
+  optionInputs[3].nextElementSibling.innerText = data.d;
+};
+
+const submitQuiz = () => {
+  const data = questions[index];
+  const ans = getAnswer();
+  console.log(ans,data.correct);
+  if (ans === data.correct) {
+    right++;
+  } else {
+    wrong++;
+  }
+  index++;
+  laodQuestion();
+  return;
+};
+
+const getAnswer = () => {
+  let answer;
+  optionInputs.forEach((input) => {
+    if (input.checked) {
+      // console.log("yes")
+      //console.log(input.value);
+      answer = input.value;
+    }
+  })
+  return answer;
+};
+
+const reset = () => {
+  optionInputs.forEach((input) => {
+    input.checked = false;
+  });
+};
+
+const endQuiz = () => {
+  document.getElementById("box").innerHTML = `
+     <div style="text-align:center; justfy-content:center;margin-top:17%">
+     <h3> Thankyou for playing the Quiz</h3>
+     <h2> ${right} / ${total} are correct </h2>
+     </div>
+     `;
+};
+
+// load ques
+laodQuestion();
